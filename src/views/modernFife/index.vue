0cae15c2a1d3ec257ce3e5c72e9d33fece6bff7f
caisheng <3026612121@qq.com>
2023-11-29 16:18:34 +0800

    fix: hooks

--- src/views/modernFife/index.vue
+++ src/views/modernFife/index.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
+     <div>
+            num1:<input v-model.number="num1" style="width: 100px" />        <br />
+            num2:<input v-model.number="num2" style="width: 100px" />    </div
+    >
+       <span>加法等于:{{ addNum }}</span>    <br />
+
     摩登生活1
   </div>
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue'
+  import { ref } from 'vue';
+  import useAdd from '@/hooks/useAdd.js';
+
+  const num1 = ref(2);
+  const num2 = ref(1);
+  const { addNum, addFn } = useAdd({ num1, num2 });
+  addFn(num1.value, num2.value);
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
